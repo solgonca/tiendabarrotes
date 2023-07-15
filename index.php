8d5a23ac075ae9506238b3499443cbde8aa56d3a
Marysol Gonzalez Caro <gonzalezsol15@hotmail.com>
2023-07-15 16:38:20 -0500

    primer commit

---
+++ index.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="style.css">
+    
+</head>
+<body>
+
+    <header class="header">
+        <div class="menu container">
+            <a href="#" class="logo">logo</a>
+            <input type="checkbox" id="menu"/>
+            <label for="menu">
+                <img src="images/menu.png" class="menu-icono" alt="menu">
+                
+            </label>
+            <nav class="navbar">
+                <ul>
+                    <li><a href="#">inicio</a></li>
+                    <li><a href="#">Domicilio</a></li>
+                    <li><a href="#">Servicios</a></li>
+                    <li><a href="#">Contacto</a></li>
+                </ul>
+
+            </nav>
+
+        </div>
+
+        <div class="header-content container">
+            <div class="header-txt">
+                <h1>Tienda de Comida<div class="progress">
+                <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
+                </div>
+                </h1>
+                <p>
+                Esta es una tienda de abarrotes
+                con mucha variedad, aquí
+                podrán encontrar todo tipo de 
+                productos tanto de aseo como de
+                comida a un buen precio
+  
+                </p>
+                <a href="#" class="btn-1">informacion</a>
+            </div>
+            <div class="header-img">
+                <img src="images/tienda.png" alt="">
+
+            </div>
+
+        </div>
+
+    </header>
+
+    <section class="about container">
+
+        <div class="about-img">
+            <img src="images/productos.png" alt="">
+        </div>
+        <div class="about-txt">
+            <h2>Nosotros</h2>
+            <p>
+                Este es un establecimiento comercial donde en compradores
+                 puedan adquirir los diferentes productos que ofrecemos.
+            </p>
+            
+            <br>
+
+            <p>
+                somos una tienda que ofrecemos toda clase de productos 
+                tanto como para aseo, todo tipo de carnes, frutas de varias
+                 clases, verduras, mekatos, y toda clase de comida etc.
+            </p>
+
+        </div>
+
+    </section>
+
+    <main class="servicios">
+        <h2>servicios</h2>
+        <div class="servicios-content container">
+
+            <div class="servicio-1">
+                <?xml version="1.0" ?><!DOCTYPE svg  PUBLIC '-//W3C//DTD SVG 1.1//EN'  'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'><svg enable-background="new 0 0 100 100" height="45px" version="1.1" viewBox="0 0 64 64" width="45px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g id="sanitizer_x2C__personal_care_products"><g><path d="M34,22H24v-8h10V22z M25,21h8v-6h-8V21z" fill="#02B1FA"/></g><g><path d="M31,15h-4V9h4V15z M28,14h2v-4h-2V14z" fill="#02B1FA"/></g><g><path d="M39.671,11.036l-2.484-0.414c-2.473-0.413-5-0.622-7.509-0.622H25.5C24.673,10,24,9.327,24,8.5    S24.673,7,25.5,7h1.533c4.338,0,8.652,0.604,12.822,1.795l1.599,0.458L39.671,11.036z M25.5,8C25.224,8,25,8.224,25,8.5    S25.224,9,25.5,9h4.179c2.564,0,5.146,0.214,7.673,0.635l1.977,0.329l0.217-0.217C35.476,8.588,31.266,8,27.033,8H25.5z" fill="#02B1FA"/></g><path d="M29.667,50H21.5c-1.378,0-2.5-1.122-2.5-2.5v-15c0-1.378,1.122-2.5,2.5-2.5h20.572v1H21.5   c-0.827,0-1.5,0.673-1.5,1.5v15c0,0.827,0.673,1.5,1.5,1.5h8.167V50z" fill="#02B1FA"/><g id="XMLID_2261_"><path d="M48.5,58h-15c-2.481,0-4.5-2.019-4.5-4.5v-7c0-2.481,2.019-4.5,4.5-4.5h15c2.481,0,4.5,2.019,4.5,4.5v7    C53,55.981,50.981,58,48.5,58z M33.5,43c-1.93,0-3.5,1.57-3.5,3.5v7c0,1.93,1.57,3.5,3.5,3.5h15c1.93,0,3.5-1.57,3.5-3.5v-7    c0-1.93-1.57-3.5-3.5-3.5H33.5z" fill="#02B1FA"/></g><g id="XMLID_2203_"><g id="XMLID_2202_"><g><path d="M44.096,32.372c0.243-2.918-0.754-5.827-2.737-7.981C39.376,22.236,36.56,21,33.632,21h-9.264      c-2.928,0-5.745,1.236-7.727,3.391c-0.458,0.498-0.858,1.04-1.208,1.609H16.5c0.042,0,0.083,0.01,0.125,0.013      c0.232-0.328,0.478-0.647,0.752-0.945C19.171,23.118,21.719,22,24.368,22h9.264c2.649,0,5.197,1.118,6.991,3.067      c1.794,1.95,2.696,4.582,2.477,7.222L42.374,41h1.003L44.096,32.372z" fill="#02B1FA"/></g><path d="M16.5,58h-10C5.673,58,5,57.327,5,56.5v-28C5,27.673,5.673,27,6.5,27h10c0.827,0,1.5,0.673,1.5,1.5v28     C18,57.327,17.327,58,16.5,58z M6.5,28C6.224,28,6,28.224,6,28.5v28C6,56.776,6.224,57,6.5,57h10c0.276,0,0.5-0.224,0.5-0.5v-28     c0-0.276-0.224-0.5-0.5-0.5H6.5z" fill="#02B1FA"/><g><path d="M15,28H8v-7h7V28z M9,27h5v-5H9V27z" fill="#02B1FA"/></g><g><path d="M14,22H9v-6h5V22z M10,21h3v-4h-3V21z" fill="#02B1FA"/></g><rect fill="##02B1FA" height="1" id="XMLID_2281_" width="1" x="12" y="18"/></g><polygon fill="#02B1FA" points="16,52 7,52 7,33 16,33 16,34 8,34 8,51 16,51   "/><g><polygon fill="#02B1FA" points="15,46 12,46 12,44 10,44 10,41 12,41 12,39 15,39 15,41 16,41 16,42 14,42 14,40 13,40 13,42      11,42 11,43 13,43 13,45 14,45 14,43 16,43 16,44 15,44    "/></g></g><g><path d="M56,16c-1.103,0-2-0.897-2-2s0.897-2,2-2s2,0.897,2,2S57.103,16,56,16z M56,13c-0.551,0-1,0.449-1,1    s0.449,1,1,1s1-0.449,1-1S56.551,13,56,13z" fill="#02B1FA"/></g><g><path d="M51.5,31c-0.827,0-1.5-0.673-1.5-1.5s0.673-1.5,1.5-1.5s1.5,0.673,1.5,1.5S52.327,31,51.5,31z M51.5,29    c-0.276,0-0.5,0.224-0.5,0.5s0.224,0.5,0.5,0.5s0.5-0.224,0.5-0.5S51.776,29,51.5,29z" fill="#02B1FA"/></g><g><path d="M52.5,8C51.673,8,51,7.327,51,6.5S51.673,5,52.5,5S54,5.673,54,6.5S53.327,8,52.5,8z M52.5,6    C52.224,6,52,6.224,52,6.5S52.224,7,52.5,7S53,6.776,53,6.5S52.776,6,52.5,6z" fill="#02B1FA"/></g><g><path d="M47,13c-1.103,0-2-0.897-2-2s0.897-2,2-2s2,0.897,2,2S48.103,13,47,13z M47,10c-0.551,0-1,0.449-1,1    s0.449,1,1,1s1-0.449,1-1S47.551,10,47,10z" fill="#02B1FA"/></g><g><path d="M46.5,24c-1.93,0-3.5-1.57-3.5-3.5s1.57-3.5,3.5-3.5c0.199,0,0.394,0.017,0.583,0.048l-0.166,0.986    C46.781,18.012,46.642,18,46.5,18c-1.378,0-2.5,1.122-2.5,2.5s1.122,2.5,2.5,2.5c0.939,0,1.791-0.519,2.223-1.354l0.889,0.459    C49.007,23.274,47.814,24,46.5,24z" fill="#02B1FA"/></g><g><path d="M50.5,21c-1.378,0-2.5-1.122-2.5-2.5s1.122-2.5,2.5-2.5s2.5,1.122,2.5,2.5S51.878,21,50.5,21z M50.5,17    c-0.827,0-1.5,0.673-1.5,1.5s0.673,1.5,1.5,1.5s1.5-0.673,1.5-1.5S51.327,17,50.5,17z" fill="#02B1FA"/></g><g><rect fill="#02B1FA" height="1" width="42.5" x="14.5" y="57"/></g><rect fill="#02B1FA" height="1" id="XMLID_2255_" width="1" x="58" y="57"/><path d="M35,53.722V52v-4c0-0.552,0.448-1,1-1h9h1h1.722c-0.347-0.595-0.984-1-1.722-1H36c-1.103,0-2,0.897-2,2v4   C34,52.737,34.405,53.375,35,53.722z" fill="#02B1FA"/></g></svg>
+                <h3>Aseo</h3>
+            </div>
+            <div class="servicio-1">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><g clip-path="url(#clip0_1651_3989)"><path d="M15.9995 11H18.0095V13H15.9995V11Z" fill="#02B1FA"/><path fill-rule="evenodd" clip-rule="evenodd" d="M5.67171 15.8841L-0.0654297 19.4699L2.92251 12L-0.0654297 4.53014L5.67171 8.11586C7.38392 5.63117 10.2506 4 13.4995 4C18.1753 4 22.0602 7.3769 22.8521 11.8247L22.8833 12L22.8521 12.1753C22.0602 16.6231 18.1753 20 13.4995 20C10.2506 20 7.38392 18.3688 5.67171 15.8841ZM6.35378 10.82L6.40964 10.8499L6.85223 10.0225L7.37754 9.182L7.36976 9.17714C8.72879 7.25364 10.9684 6 13.4995 6C17.1274 6 20.1553 8.57672 20.8497 12C20.1553 15.4233 17.1274 18 13.4995 18C10.9684 18 8.72879 16.7464 7.36976 14.8229L7.37754 14.818L6.85223 13.9775L6.40964 13.1501L6.35378 13.18L6.31755 13.122L5.45871 13.6588L4.6461 14.0935L4.67544 14.1483L4.06452 14.5301L5.07658 12L4.06452 9.46986L4.67544 9.85168L4.6461 9.90654L5.45871 10.3412L6.31755 10.878L6.35378 10.82Z" fill="#02B1FA"/></g><defs><clipPath id="clip0_1651_3989"><rect width="24" height="24" fill="white"/></clipPath></defs></svg>
+                <h3>Carnes</h3>
+            </div>
+
+            <div class="servicio-1">
+            <?xml version="1.0" ?><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><path d="M6 8H8.01V10H6V8Z" fill="#02B1FA"/><path d="M11 11H13.01V13H11V11Z" fill="#02B1FA"/><path d="M8 15H10.01V17H8V15Z" fill="#02B1FA"/><path d="M15 15H17.01V17H15V15Z" fill="#02B1FA"/><path fill-rule="evenodd" clip-rule="evenodd" d="M1 12C1 5.92487 5.92487 1 12 1C12.0366 1 12.0732 1.00018 12.1097 1.00054L13.3208 1.01239L13.08 2.19932C13.0276 2.45721 13 2.72486 13 3C13 4.95769 14.4074 6.58878 16.2659 6.93296L16.9419 7.05815L17.067 7.73414C17.4112 9.59261 19.0423 11 21 11C21.2751 11 21.5428 10.9724 21.8007 10.92L22.9876 10.6792L22.9995 11.8903C22.9998 11.9268 23 11.9634 23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12ZM11.0002 3.0549C6.50018 3.55223 3 7.36736 3 12C3 16.9706 7.02944 21 12 21C16.6326 21 20.4478 17.4998 20.9451 12.9998C18.2609 12.9757 15.9991 11.1899 15.2573 8.74272C12.8101 8.00085 11.0243 5.73912 11.0002 3.0549Z" fill="#02B1FA"/></svg>
+                <h3>Mekatos</h3>
+            </div>
+            <div class="servicio-1">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: #02B1FA; transform: ;msFilter:;"><path d="m11.13 4.41 4.23 4.23L14.3 9.7l-4.24-4.24-1.77 1.77 4.24 4.24-1.06 1.06-4.24-4.24-1.77 1.77L9.7 14.3l-1.06 1.06-4.23-4.23C1.86 14 1.55 18 3.79 20.21a5.38 5.38 0 0 0 3.85 1.5 8 8 0 0 0 5.6-2.47l6-6c2.87-2.87 3.31-7.11 1-9.45s-6.24-1.93-9.11.62z"></path></svg>
+                <h3>Comida</h3>
+            </div>
+
+        </div>
+
+    </main>
+
+
+<section class="formulario container">
+
+    <form method="post" autocomplete="off">
+
+         <h2>Datos personales</h2>
+
+         <div class="input-group">
+
+            <div class="input-container">
+                <input type="text" name="name" placeholder="Nombre y Apellido">
+                <i class="fa-solid fa-user"></i>
+            </div>
+            <div class="input-container">
+                <input type="tel" name="phone" placeholder="Telefono Celular">
+                <i class="fa-solid fa-phone"></i>
+            </div>
+            <div class="input-container">
+                <input type="email" name="email" placeholder="Correo">
+                <i class="fa-solid fa-envelope"></i>
+            </div>
+            <div class="input-container">
+               <textarea name="message" placeholder="Mensaje"></textarea> 
+            </div>
+        
+            <input type="submit" name="send" class="btn" onClick="myFunction()">
+            
+            
+            
+
+
+         </div>
+
+    </form> 
+</section>
+
+<footer class="footer">
+    <div class="footer-content container">
+        <div class="link
+             <a href="#" class="logo">logo</a>
+
+        </div>
+        
+        <div class="link">
+            <ul>
+                <li><a href="#">inicio</a></li>
+                <li><a href="#">Domicilio</a></li>
+                <li><a href="#">Servicios</a></li>
+                <li><a href="#">Contacto</a></li>
+            </ul>
+
+        </div>
+
+    </div>
+
+</footer>
+
+<?php
+    include("send.php");
+?>
+
+<script>
+    function myFunction() {
+        window.location.href="http://localhost/pagina"
+    }
+</script>
+
+</body>
+</html>
